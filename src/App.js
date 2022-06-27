e89984946c53eb55bc9a2fc60932d75654c41cf9
unknown <hannanmalik1999@gmail.com>
2022-06-28 00:59:08 +0200

    Save dice state by clicking

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from "react"
 import "./app.css"
 import Die from "../src/Components/Die.js"
+import {nanoid} from "nanoid"
 
-function App() {
-  const [dice, setDice] = React.useState(allNewDice())
+  function App() {
+    const [dice, setDice] = React.useState(allNewDice())
+
+  function generateNewDie() {
+      return {
+        value :Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+        }
+    }
   
   function allNewDice() {
     const newDice = []
     for(let i=0; i< 10;i++) {
-      newDice.push(Math.ceil(Math.random() * 6))
+    newDice.push(generateNewDie())
     }
     return newDice
   }
-    const diceElemets = dice.map(die => <Die value={die} />)
+
+  function rollDice() {
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ?
+      die: generateNewDie()
+    }))
+  }
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ? 
+      {...die, isHeld: !die.isHeld} :
+      die
+    }))
+  }
+
+    const diceElemets = dice.map(die => (
+    <Die key={die.id} value={die.value} isHeld={die.isHeld}
+    holdDice={() => holdDice(die.id)}/>
+    ))
    
     return(
         <main>
           <div className="dice-container">
           {diceElemets}
           </div>
+          <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
     )
 }
--- src/Components/Die.js
+++ src/Components/Die.js
@@ -2,8 +2,11 @@ import React from "react"
 import "../styles/die.css"
 
 function Die (props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "White"
+    }
     return(
-        <div className="dice-box">
+        <div onClick={props.holdDice} className="dice-box" style={styles}>
         <h2 className="dice-num">{props.value}</h2>
         </div>
     )
