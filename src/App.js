64ab497a24085d449c46453b6e6d7d2ab5cc242b
unknown <hannanmalik1999@gmail.com>
2022-06-27 14:48:24 +0200

    Random numbers for the dices

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "./app.css"
 import Die from "../src/Components/Die.js"
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+  
+  function allNewDice() {
+    const newDice = []
+    for(let i=0; i< 10;i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+    const diceElemets = dice.map(die => <Die value={die} />)
+   
     return(
         <main>
           <div className="dice-container">
-          <Die value ="1"/>
-          <Die value ="2"/>
-          <Die value ="3"/>
-          <Die value ="4"/>
-          <Die value ="5"/>
-          <Die value ="6"/>
-          <Die value ="7"/>
-          <Die value ="8"/>
-          <Die value ="9"/>
-          <Die value ="1"/>
+          {diceElemets}
           </div>
         </main>
     )
